805a467c7f0815ae5e718cf7624ccca2510cf2a8
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-05-09 13:34:24 -0400

    fixed rookie mistake. forgot '=>' in one of the production statements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise mailer
-  config.action_mailer.default_url_options = { :host 'school-io.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'school-io.herokuapp.com' }
 end
